0c7714b8c7986980af1b64e8f85fd4ebc1eecbef
bwoodward523 <bwoodward@ycp.edu>
2025-10-22 19:42:34 -0400

    iCloud

--- CloudAPI.py
+++ CloudAPI.py
@@ -2,9 +2,24 @@
 import requests
 import json
 from logs.logger_config import get_logger
-
+from iCloud import iCloud
 # Get logger for this module
 logger = get_logger(__name__)
+
+class CloudAPI(iCloud):
+    def get_test_data(self):
+        logger.info("Starting API test data request")
+        url = "https://api.revmetrix.io/api/gets/Test"
+        result = make_get_request(url)
+        
+        if 'error' in result:
+            logger.error(f"API request failed: {result['error']}")
+            return result['status_code'], result['error']
+        else:
+            logger.info("API request successful")
+            logger.debug(f"Retrieved data: {result['data']}")
+            print(f"Data: {result['data']}")
+            return result['status_code'], result['data']
 
 def make_get_request(url):
     """
--- iCloud.py
+++ iCloud.py
@@ -1,4 +1,4 @@
-
+from abc import ABC, abstractmethod
 import requests
 import json
 from logs.logger_config import get_logger
@@ -8,3 +8,7 @@ logger = get_logger(__name__)
 #Turn into an API interface with a bunch of abstract methods
 
 #Make cloudAPI implement iCloud
+class iCloud(ABC):
+    @abstractmethod
+    def get_test_data(self):
+        pass
