820fce36791f2227d4619301c547a159a4eb9b67
cmack6 <cmack6@ycp.edu>
2025-10-23 19:33:52 -0400

    implementing bdc motor
    working on it, but program wont run right now

--- Motors/BDCMotor.py
+++ Motors/BDCMotor.py
@@ -1,14 +1,28 @@
 from .iMotor import iMotor
-import RPi.GPIO as GPIO
+from gpiozero import PWMOutputDevice
+import time
+
+MIN_THR = 0.050
+MID_THR = 0.075
+MAX_THR = 0.100
+STEP = 0.002   # ~2 per-mille
+FREQ = 50
 
 class BDCMotor(iMotor):
     motorID = 0
     currSpeed = 0.0
     targetSpeed = 0.0
     targetPower = 0.0
+    GPIO_Pin = 26; #GPIO Pin for the Motor
+    motor = PWMOutputDevice(GPIO_Pin, frequency=FREQ)
+
+    def clamp(x, lo, hi):
+        return max(lo, min(hi, x))
      
     def __init__(self, GPIOPin : int):
-        pass
+        thr = MID_THR
+        self.motor.value = 0.0
+
 
     # Turns on Motor at Specified Power (Duty Cycle)
     def start(self, dutyCycle = 100):
@@ -18,7 +32,7 @@ class BDCMotor(iMotor):
         pass
 
     def changeSpeed(self, dutyCycle : int):
-        pass
+        dutyCycle = self.clamp(dutyCycle, 0, 100)
 
     def int getCurrentSpeed(self):
         pass
