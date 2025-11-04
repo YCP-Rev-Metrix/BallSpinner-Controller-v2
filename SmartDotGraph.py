bfe10e5a7fd5eaeebeb3d573e0388bf885092f58
mattbrown64 <mattbrown4404@gmail.com>
2025-11-04 14:41:21 -0500

    Added modes to smartdot graph

--- SmartDotGraph.py
+++ SmartDotGraph.py
@@ -1,3 +1,4 @@
+from unittest import case
 from PyQt6 import QtWidgets, uic
 import pyqtgraph as pg
 import numpy as np
@@ -37,26 +38,7 @@ class SmartDotGraph(QtWidgets.QWidget):
         self.graph.setMouseEnabled(x=False, y=False)
         legend = self.graph.addLegend()
         legend.setColumnCount(3)
-  
-       
-        """
-        # Connect checkbox state changes to update_graph method
-        self.chkAccelerometer_X.stateChanged.connect(self.update_graph)
-        self.chkAccelerometer_Y.stateChanged.connect(self.update_graph)
-        self.chkAccelerometer_Z.stateChanged.connect(self.update_graph)
-        self.chkGyroscope_X.stateChanged.connect(self.update_graph)
-        self.chkGyroscope_Y.stateChanged.connect(self.update_graph)
-        self.chkGyroscope_Z.stateChanged.connect(self.update_graph)
-        self.chkMagnetometer_X.stateChanged.connect(self.update_graph)
-        self.chkMagnetometer_Y.stateChanged.connect(self.update_graph)
-        self.chkMagnetometer_Z.stateChanged.connect(self.update_graph)
-        self.chkLight.stateChanged.connect(self.update_graph)
-        self.update_graph()  # Initial graph update
 
-        #Limit view checkbox
-        self.chkLimitView.stateChanged.connect(self.toggle_limit_view)
-        self.dsbLookBackSeconds.valueChanged.connect(self.toggle_limit_view)
-                """
 
         #Select/Deselect All buttons
         self.btnSelectAll.clicked.connect(self.select_all)
@@ -121,9 +103,116 @@ class SmartDotGraph(QtWidgets.QWidget):
             self.graph.setXRange(last - self.dsbLookBackSeconds.value(), last)
         else:
             self.graph.enableAutoRange(axis='x')
-
-
-
+    def updateAccelerometer(self, time, x, y, z):
+        if self.chkAccelerometer_X.isChecked():
+            self.graph.plot(time, x, pen=pg.mkPen(color='r', width=2), name='Accelerometer_X')
+        if self.chkAccelerometer_Y.isChecked():
+            self.graph.plot(time, y, pen=pg.mkPen(color='g', width=2), name='Accelerometer_Y')
+        if self.chkAccelerometer_Z.isChecked():
+            self.graph.plot(time, z, pen=pg.mkPen(color='b', width=2), name='Accelerometer_Z')
+        last = time[-1]
+        if self.chkLimitView.isChecked():
+            self.graph.setXRange(last - self.dsbLookBackSeconds.value(), last)
+        else:
+            self.graph.enableAutoRange(axis='x')
+    def updateGyroscope(self, time, x, y, z):
+        if self.chkGyroscope_X.isChecked():
+            self.graph.plot(time, x, pen=pg.mkPen(color='c', width=2), name='Gyroscope_X')
+        if self.chkGyroscope_Y.isChecked():
+            self.graph.plot(time, y, pen=pg.mkPen(color='m', width=2), name='Gyroscope_Y')
+        if self.chkGyroscope_Z.isChecked():
+            self.graph.plot(time, z, pen=pg.mkPen(color='y', width=2), name='Gyroscope_Z')
+        last = time[-1]
+        if self.chkLimitView.isChecked():
+            self.graph.setXRange(last - self.dsbLookBackSeconds.value(), last)
+        else:
+            self.graph.enableAutoRange(axis='x')
+    def updateMagnetometer(self, time, x, y, z):
+        if self.chkMagnetometer_X.isChecked():
+            self.graph.plot(time, x, pen=pg.mkPen(color="#008080", width=2), name='Magnetometer_X')
+        if self.chkMagnetometer_Y.isChecked():
+            self.graph.plot(time, y, pen=pg.mkPen(color="#800000", width=2), name='Magnetometer_Y')
+        if self.chkMagnetometer_Z.isChecked():
+            self.graph.plot(time, z, pen=pg.mkPen(color='#800080', width=2), name='Magnetometer_Z')
+        last = time[-1]
+        if self.chkLimitView.isChecked():
+            self.graph.setXRange(last - self.dsbLookBackSeconds.value(), last)
+        else:
+            self.graph.enableAutoRange(axis='x')
+    def updateLight(self, time, value):
+        if self.chkLight.isChecked():
+            self.graph.plot(time, value, pen=pg.mkPen(color='w', width=2), name='Light')
+        last = time[-1]
+        if self.chkLimitView.isChecked():
+            self.graph.setXRange(last - self.dsbLookBackSeconds.value(), last)
+        else:
+            self.graph.enableAutoRange(axis='x')
+    def setMode(self, mode):
+        match mode:
+            case 'Accelerometer':
+                self.chkAccelerometer_X.setVisible(True)
+                self.chkAccelerometer_Y.setVisible(True)
+                self.chkAccelerometer_Z.setVisible(True)
+                self.chkGyroscope_X.setVisible(False)
+                self.chkGyroscope_Y.setVisible(False)
+                self.chkGyroscope_Z.setVisible(False)
+                self.chkMagnetometer_X.setVisible(False)
+                self.chkMagnetometer_Y.setVisible(False)
+                self.chkMagnetometer_Z.setVisible(False)
+                self.chkLight.setVisible(False)
+                # uncheck all other checkboxes
+                self.deselect_all()
+            case 'Gyroscope':
+                self.chkAccelerometer_X.setVisible(False)
+                self.chkAccelerometer_Y.setVisible(False)
+                self.chkAccelerometer_Z.setVisible(False)
+                self.chkGyroscope_X.setVisible(True)
+                self.chkGyroscope_Y.setVisible(True)
+                self.chkGyroscope_Z.setVisible(True)
+                self.chkMagnetometer_X.setVisible(False)
+                self.chkMagnetometer_Y.setVisible(False)
+                self.chkMagnetometer_Z.setVisible(False)
+                self.chkLight.setVisible(False)
+                # uncheck all other checkboxes
+                self.deselect_all()
+            case 'Magnetometer':
+                self.chkAccelerometer_X.setVisible(False)
+                self.chkAccelerometer_Y.setVisible(False)
+                self.chkAccelerometer_Z.setVisible(False)
+                self.chkGyroscope_X.setVisible(False)
+                self.chkGyroscope_Y.setVisible(False)
+                self.chkGyroscope_Z.setVisible(False)
+                self.chkMagnetometer_X.setVisible(True)
+                self.chkMagnetometer_Y.setVisible(True)
+                self.chkMagnetometer_Z.setVisible(True)
+                self.chkLight.setVisible(False)
+                 # uncheck all other checkboxes
+                self.deselect_all()
+            case 'Light':
+                self.chkAccelerometer_X.setVisible(False)
+                self.chkAccelerometer_Y.setVisible(False)
+                self.chkAccelerometer_Z.setVisible(False)
+                self.chkGyroscope_X.setVisible(False)
+                self.chkGyroscope_Y.setVisible(False)
+                self.chkGyroscope_Z.setVisible(False)
+                self.chkMagnetometer_X.setVisible(False)
+                self.chkMagnetometer_Y.setVisible(False)
+                self.chkMagnetometer_Z.setVisible(False)
+                self.chkLight.setVisible(True)
+                 # uncheck all other checkboxes
+                self.deselect_all()
+            case _:
+                # Show all checkboxes
+                self.chkAccelerometer_X.setVisible(True)
+                self.chkAccelerometer_Y.setVisible(True)
+                self.chkAccelerometer_Z.setVisible(True)
+                self.chkGyroscope_X.setVisible(True)
+                self.chkGyroscope_Y.setVisible(True)
+                self.chkGyroscope_Z.setVisible(True)
+                self.chkMagnetometer_X.setVisible(True)
+                self.chkMagnetometer_Y.setVisible(True)
+                self.chkMagnetometer_Z.setVisible(True)
+                self.chkLight.setVisible(True)
 
 
 if __name__ == '__main__':
@@ -135,6 +224,8 @@ if __name__ == '__main__':
     window = SmartDotGraph()
     window.setWindowTitle("Smart Dot Graph")
     window.show()
+
+    window.setMode('Gyroscope')
     
     # Start the event loop
     sys.exit(app.exec())
