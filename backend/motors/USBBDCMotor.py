6ffab2b91cf09bc063071972d413149a1bf7ab77
cmack6 <cmack6@ycp.edu>
2025-11-20 00:43:24 -0500

    logging encoder values?
    maybe?

--- backend/motors/USBBDCMotor.py
+++ backend/motors/USBBDCMotor.py
@@ -6,6 +6,8 @@ from pyvesc import encode
 from pyvesc.VESC.messages import SetDutyCycle
 
 from .iMotor import iMotor
+from logs.logger_config import get_logger
+logger = get_logger(__name__)
 
 PORT = "/dev/ttyACM0"   # or "/dev/ttyUSB0"
 BAUD = 115200
@@ -204,11 +206,27 @@ class USBBDCMotor(iMotor):
         self.currSpeed = self.targetSpeed
 
     def getCurrentSpeed(self):
-        send_get_values(ser)
+        '''send_get_values(ser)
         vals = read_mc_values(ser)
         if vals is not None:
             
-            return vals["rpm"]/2.0 #Encoder implement, maybe working
+            return vals["rpm"]/2.0 #Encoder implement, maybe working'''
+        send_get_values(ser)
+        vals = read_mc_values(ser)
+        if vals is not None:
+            erpm = vals["rpm"]
+
+            # Your 4-pole RC motor = 2 pole pairs → mech RPM = ERPM / 2
+            mech_rpm = erpm / 2.0
+
+
+        logger.info(f"ERPM: {erpm:9.1f} |
+                RPM: {mech_rpm:9.1f} | 
+                I_motor: {vals['motor_current']:6.2f} A | 
+                V_in: {vals['v_in']:5.2f} V | 
+                Duty: {vals['duty_now']*100:5.1f}% | 
+                Fault: {vals['fault']}"
+            )
 
     def rampUp(self):
         while self.currSpeed < self.targetSpeed:
