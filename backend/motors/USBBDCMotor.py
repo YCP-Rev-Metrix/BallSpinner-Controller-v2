e232ae66723b2323beb067f0ffe404926d66adc5
mattbrown64 <mattbrown4404@gmail.com>
2026-03-24 10:13:40 -0400

    Limit low speed kick

--- backend/motors/USBBDCMotor.py
+++ backend/motors/USBBDCMotor.py
@@ -290,7 +290,7 @@ class USBBDCMotor(iMotor):
         if self.currSpeed < self.targetSpeed**2/1200 and self.targetSpeed != 0:
             # Motor is stopped give big kick to get it going, then let PID take over
             command = 1/self.duty_cycle_scale  # Garbage for logging purposes since we're not really using the PID output for this case
-            duty = 1  # run at 100% duty until we get a speed reading, then PID can take over
+            duty = min(self.targetSpeed/600, 1.0)  # run at 100% duty until we get a speed reading, then PID can take over
         else:
             command = (
                 self.targetSpeed
