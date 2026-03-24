3ac8332f2d0cb88483c31fb6bf36140a06648bdf
mattbrown64 <mattbrown4404@gmail.com>
2026-03-24 11:58:00 -0400

    Moved scale to cutoff

--- backend/motors/USBBDCMotor.py
+++ backend/motors/USBBDCMotor.py
@@ -286,12 +286,13 @@ class USBBDCMotor(iMotor):
 
         self._prev_error = error
         self._prev_time = now
+        self.highspeedScale = 1.0
+        if self.targetSpeed > 300:
+            self.highspeedScale = 1.5  # Give extra boost at higher speeds to help overcome friction losses
         #target speed * targetspeed/(Max speed) The higher the target speed, the more aggressive the PID output, with a little extra boost as we approach max speed to help overcome friction losses. Adjust the +200 term as needed based on your motor's characteristics.
-        if self.currSpeed < self.targetSpeed**2/1200 and self.targetSpeed != 0:
+        if self.currSpeed < max(self.targetSpeed*self.highspeedScale, 1200)**2/1200 and self.targetSpeed != 0:
             # Motor is stopped give big kick to get it going, then let PID take over
             command = 1/self.duty_cycle_scale  # Garbage for logging purposes since we're not really using the PID output for this case
-            if self.targetSpeed >= 300:
-                self.targetSpeed *= 1.5 # Give extra boost at higher speeds to help overcome friction losses, which are more significant at higher speeds. Adjust the 1.5 factor as needed based on testing with your specific motor and load.
             duty = min(self.targetSpeed/6000, 1.0)  # run at 100% duty until we get a speed reading, then PID can take over
         else:
             command = (
